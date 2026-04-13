973b610ff11feb92beaa1bc9c5ea352121e2cd04
wvvtop <sav.sanya00@mail.ru>
2026-04-13 15:12:00 +0700

    Исправил валидацию

--- src/registration/RegistrationForm.vue
+++ src/registration/RegistrationForm.vue
@@ -6,18 +6,39 @@ import ButtonComponent from '@/components/ButtonComponent.vue'
 import { useAuthStore } from '@/stores/auth'
 
 const router = useRouter()
-const authStore = useAuthStore();
+const authStore = useAuthStore()
+
 const email = ref('')
 const password = ref('')
 const errorMsg = ref('')
 const loading = ref(false)
 
+const validate = () => {
+  if (!email.value.includes('@')) {
+    errorMsg.value = 'Введите корректный email'
+    return false
+  }
+
+  if (password.value.length < 6) {
+    errorMsg.value = 'Пароль должен быть минимум 6 символов'
+    return false
+  }
+
+  errorMsg.value = ''
+  return true
+}
+
 const handleSubmit = async () => {
+  if (!validate()) return
+
   try {
+    loading.value = true
     await authStore.register(email.value, password.value)
     router.push('/login')
   } catch (err) {
-    errorMsg.value = authStore.error
+    errorMsg.value = authStore.error || 'Ошибка регистрации'
+  } finally {
+    loading.value = false
   }
 }
 </script>
